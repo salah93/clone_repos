6f32f4924dc28d11f52fe473b5304ef3c6878130
salah ahmed <sa2484@nyu.edu>
2017-02-01 09:17:07 -0500

    add argument parser

--- clone_repo.py
+++ clone_repo.py
@@ -1,18 +1,28 @@
+import argparse
 import subprocess
 from os.path import join, expanduser
 
 import requests
 
 
-def clone_repos(user, directory):
-    gh_url = 'https://github.com/'
+def clone_repos(user):
     user_url = 'https://api.github.com/users/{user}/repos'.format(user=user)
     page = requests.get(user_url)
     if page.status_code >= 400:
         raise Exception
     repos = filter(lambda x: not x['fork'], page.json())
-    [subprocess.Popen(['git', 'clone', join(gh_url, i['full_name'])], cwd=expanduser(directory)) for i in repos]
-
+    return repos
 
 if __name__ == '__main__':
-    clone_repos('salah93', '~/Projects')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory',
+                        type=str, default='~/Projects')
+    parser.add_argument('--user',
+                        type=str, default=salah93)
+    args = parser.parse_args()
+    repos = clone_repos(args.user)
+    gh_url = 'git@github.com:'
+    for r in repos:
+        command = ['git', 'clone', gh_url + r['full_name']]
+        subprocess.Popen(command, cwd=expanduser(args.directory))
+)
