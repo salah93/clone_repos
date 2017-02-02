e678a6f7151605f1c3707ee84dda5225f7609803
salah ahmed <sa2484@nyu.edu>
2017-02-01 19:14:38 -0500

    add support for private repos

--- clone_repo.py
+++ clone_repo.py
@@ -1,28 +1,32 @@
 import argparse
+import json
 import subprocess
+from os import environ
 from os.path import join, expanduser
 
 import requests
 
 
-def clone_repos(user):
-    user_url = 'https://api.github.com/users/{user}/repos'.format(user=user)
-    page = requests.get(user_url)
+def clone_repos(affiliation):
+    user_url = 'https://api.github.com/user/repos'
+    page = requests.get(user_url, dict(affiliation=','.join(affiliation), access_token=environ['GITHUB_TOKEN']))
     if page.status_code >= 400:
         raise Exception
     repos = filter(lambda x: not x['fork'], page.json())
     return repos
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory',
                         type=str, default='~/Projects')
-    parser.add_argument('--user',
-                        type=str, default=salah93)
+    # you can also add 'collaborator', 'organization_member'
+    parser.add_argument('--affiliation',
+                        type=str, nargs='+', 
+                        default=['owner'])
     args = parser.parse_args()
-    repos = clone_repos(args.user)
+    repos = clone_repos(args.affiliation)
     gh_url = 'git@github.com:'
     for r in repos:
         command = ['git', 'clone', gh_url + r['full_name']]
         subprocess.Popen(command, cwd=expanduser(args.directory))
-)
